5756dfab047b052f088b7d1b9b569ed554a09fff
danieljaraba <daesjaga2003@gmail.com>
2024-04-22 23:32:33 -0500

    Dockerfile with env variables

---
+++ README.md
@@ -0,0 +1,3 @@
+It's a Ecommerce Copy
+
+## MAKE SURE YOU HAVE [ADMIN](https://github.com/ChristianFlor/ecommerce-admin) SETUP FIRST!
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard(process.env.BILLOBARD_ID ?? 'b70a5156-ffd5-4334-add9-2f6490c09020');
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
